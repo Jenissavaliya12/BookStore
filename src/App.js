44e90dc561aed7af53c74b773aefaeb23c490d27
Jenissavaliya12 <savaliyajenis12@gmail.com>
2023-03-09 14:16:47 +0530

    Day 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from "react";
 import "./App.css";
+import About from "./pages/About";
 // import Title from "./component/Title";
-// import  Home from "./pages/Home"
-import Counter from "./component/Counter"
+import  Home from "./pages/Home"
+// import Counter from "./component/Counter"
 function App() {
+  const [page, setPage] = React.useState("Home");
+
+  const handleChange = () => {
+    if (page === "Home") {
+      setPage("About");
+    } else {
+      setPage("Home");
+    }
+  };
   return (
-    <div>
-    <Counter />
-      
+    <div className="container">
+    {page === "Home" ? <Home/> : <About/>}
+    <button onClick={handleChange}>Change Page</button>
+      {/* <Counter /> */}
+
       {/* <Home />
 
       <Title title="react" name="jenis"/>
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className='container'>
+        This is About Page.
+      </div>
+    )
+  }
+}
