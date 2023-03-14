14fb99de6207d02d302f776d0135298494101e42
Jenissavaliya12 <savaliyajenis12@gmail.com>
2023-03-15 00:06:35 +0530

    Day 14 Completed

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import React from "react";
 import "./App.css";
-import CounterHook from "./component/CounterHook";
-// import Consumer3 from "./component/Consumer3";
-// import { UserProvider } from "./component/UserContext";
-// import Users from "./component/Users";
-// import Post from "./component/Post";
-// import RenderPropsComp from "./component/RenderPropsComp";
-// import FocusInputRef from "./component/FocusInputRef";
-// import RenderPropsComponent from "./component/RenderPropsComponent";
-// import InputRef from "./component/InputRef";
-// import LogInForm from "./component/LogInForm";
-// import About from "./pages/About";
-// import Title from "./component/Title";
-// import Home from "./pages/Home";
-// import Counter from "./component/Counter"
-// import List from "./component/List"
-function App() {
-  // const [page, setPage] = React.useState("Home");
 
-  // const handleChange = () => {
-  //   if (page === "Home") {
-  //     setPage("About");
-  //   } else {
-  //     setPage("Home");
-  //   }
+import LogInForm from "./component/LogInForm";
+
+function App() {
+  
   return (
     <div>
-     <CounterHook />
-     {/* <Post /> */}
-     {/* <Users/> */}
-     
-     
-      {/* <UserProvider value="Jenis">
-        <Consumer3 />
-      </UserProvider> */}
 
-
-
-      {/* <Home /> */}
-
-      {/* <RenderPropsComp
-        render={() => {
-          return <h3>I am coming from Render props</h3>;
-        }}
-      /> */}
-      {/* <FocusInputRef /> */}
-      {/* <InputRef /> */}
-
-      {/* <List /> */}
-
-      {/* <LogInForm/> */}
-
-      {/* <Title title="Jenis" description="Web Developer"/> */}
-      {/* {page === "Home" ? <Home/> : <About/>}
-    <button onClick={handleChange}>Change Page</button> */}
-      {/* <Counter /> */}
-
-      {/* <Home />
-
-      <Title title="react" name="jenis"/>
-      <Title name="Manthan" title="express"/>
-      <Title name="Sandip" title="node"/> */}
+    <LogInForm />
+   
     </div>
   );
 }
