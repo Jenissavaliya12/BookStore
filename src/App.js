a21c04644f089a80832cb0e9cbb7f049611ab3a6
Jenissavaliya12 <savaliyajenis12@gmail.com>
2023-03-18 00:14:21 +0530

    Day 18 useContext

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./App.css";
 // import HookMouse from "./component/HookMouse";
-import PostDetail from "./component/PostDetail";
-
+// import PostDetail from "./component/PostDetail";
+import Consumer2 from "./component/Consumer2";
 // import LogInForm from "./component/LogInForm";
 // import Users from "./component/Users";
 
+export const UserContext = React.createContext();
+export const channelContext = React.createContext();
+
 function App() {
-  
   return (
     <div>
-    <PostDetail />
-    {/* <HookMouse /> */}
-    {/* <Users /> */}
-    {/* <LogInForm /> */}
-   
+      <UserContext.Provider value={"Jenis"}>
+        <channelContext.Provider value={"Sankalp"}>
+          <Consumer2 />
+        </channelContext.Provider>
+      </UserContext.Provider>
+      {/* <PostDetail /> */}
+      {/* <HookMouse /> */}
+      {/* <Users /> */}
+      {/* <LogInForm /> */}
     </div>
   );
 }
--- src/component/Consumer2.js
+++ src/component/Consumer2.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
-import Consumer3 from "./Consumer2"
- class Consumer2 extends Component {
-  render() {
-    return (
-      <div>
-        <Consumer3 />       
-      </div>
-    )
-  }
+import React, { useContext } from "react";
+// import Consumer3 from "./Consumer2"
+import { UserContext, channelContext } from "../App";
+//  class Consumer2 extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Consumer3 />
+//       </div>
+//     )
+//   }
+// }
+
+function Consumer2() {
+  const user = useContext(UserContext);
+  const channel = useContext(channelContext);
+  return (
+    <div className="text-center">
+      Hello {user} , Hello {channel} 
+    </div>
+    // <UserContext.Consumer>
+    //   {
+    //     user => {
+    //      return (
+    //       <channelContext.Consumer>
+    //         {
+    //           channel => {
+    //             return (
+    //               <div className='text-center'> Hello {user}  , Hello {channel}</div>
+    //             )
+    //           }
+    //         }
+    //       </channelContext.Consumer>
+    //      )
+    //     }
+    //   }
+    // </UserContext.Consumer>
+  );
 }
 
-export default Consumer2
+export default Consumer2;
